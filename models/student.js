def4dac044fb2e7170de1c7f4a06cedac925219e
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-08-19 19:16:34 +0530

    Creating Student model

---
+++ models/student.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose'); //import mongoose module
+
+// create the schema for student
+const studentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    batch: {
+        type: String,
+        required: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['placed' , 'not_placed'],
+        default: 'not_placed'
+    },
+    DSA_FinalScore :{
+        type:Number,
+        default:0
+    },
+    WebD_FinalScore :{
+        type : Number,
+        default:0
+    },
+    React_FinalScore :{
+        type : Number,
+        default:0
+    }
+}, {
+    timestamps: true
+});
+
+// create the Student collection/model
+const Student = mongoose.model('Student', studentSchema);
+
+// export the model
+module.exports = Student;
+
