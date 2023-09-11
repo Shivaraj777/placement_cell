1c5cf580eeaa78731364528a4a1e71d7b9a812a0
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-09-11 19:27:44 +0530

    render deployment changes

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,14 @@
-
+// config for dev mode
 const development = {
     name: 'development',
     mongo_CONNECT_URL: 'mongodb://127.0.0.1/placement_cell'
 }
 
+// config for prod mode
 const production = {
     name: 'production',
-    mongo_CONNECT_URL: 'mongodb+srv://codeslayer09876:XoSzERBq8JPOWnf8@placementcell.3ydcrhc.mongodb.net/?retryWrites=true&w=majority'
+    mongo_CONNECT_URL: 'mongodb+srv://codeslayer09876:XoSzERBq8JPOWnf8@placementcell.3ydcrhc.mongodb.net/PlacementCell?retryWrites=true&w=majority'
 }
 
-module.exports = production;
-// module.exports = eval(process.env.PLACEMENT_CELL_ENV) == undefined ? development : eval(process.env.PLACEMENT_CELL_ENV);
+// module.exports = production;
+module.exports = eval(process.env.PLACEMENT_CELL_ENV) == undefined ? development : eval(process.env.PLACEMENT_CELL_ENV);
