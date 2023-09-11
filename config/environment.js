7b80167ce599165065ed202786174243523aba79
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-09-11 19:02:21 +0530

    testing for deployment

--- config/environment.js
+++ config/environment.js
@@ -9,4 +9,5 @@ const production = {
     mongo_CONNECT_URL: 'mongodb+srv://codeslayer09876:XoSzERBq8JPOWnf8@placementcell.3ydcrhc.mongodb.net/?retryWrites=true&w=majority'
 }
 
-module.exports = eval(process.env.PLACEMENT_CELL_ENV) == undefined ? development : eval(process.env.PLACEMENT_CELL_ENV);
+module.exports = production;
+// module.exports = eval(process.env.PLACEMENT_CELL_ENV) == undefined ? development : eval(process.env.PLACEMENT_CELL_ENV);
