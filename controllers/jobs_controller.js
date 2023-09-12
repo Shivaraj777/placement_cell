bc0fee8015b81a5e9a35fe7b9ee06166def167c3
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-09-12 16:51:53 +0530

    changes to job controller

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch'); //import node-fetch module
-const {jobsData} = require('../data'); //import the data module(test purpose)
+// const {jobsData} = require('../data'); //import the data module(test purpose)
 
 // fetch the jobs from remotive API
 module.exports.jobPage = async function (req, res) {
     try{
-        // const response = await fetch('https://remotive.com/api/remote-jobs');
-        // const jobsData = await response.json();
+        const response = await fetch('https://remotive.com/api/remote-jobs');
+        const jobsData = await response.json();
         // console.log(jobsData);
 
         return res.render('jobs', {
